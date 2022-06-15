33af40852dcbb9dbab2e15b83fd767c1df5a733f
Christopher Palileo <cpalileo@gmail.com>
2022-06-15 10:15:53 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// 1. Create two variables, firstCard and secondCard.
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = Math.floor(Math.random() * 12) + 2;
+let secondCard = Math.floor(Math.random() * 12) + 2;
+
+console.log("Your first card is " + firstCard);
+console.log("Your second card is " + secondCard);
