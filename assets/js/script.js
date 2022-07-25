41635d48a286db55569760e5753aa5bdc1b0e06c
Christopher Palileo <cpalileo@gmail.com>
2022-07-25 14:37:13 -0700

    clean vsc src ctrl

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,74 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
+let firstCard = null
+let secondCard = null
+let sum = 0
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
 
-let firstCard = Math.floor(Math.random() * 12) + 2;
-let secondCard = Math.floor(Math.random() * 12) + 2;
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    let cards = [firstCard, secondCard]
+    let sum = cards[]
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
+    renderGame()
+}
 
-console.log("Your first card is " + firstCard);
-console.log("Your second card is " + secondCard);
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+while (condition) {
+    
+}
+
+do {
+    
+} while (condition);
